0712588dc5c57943f524320de6a92243377c6fe1
vedam-parthiv-kumar <vedamparthivkumar06@gmail.com>
2026-03-17 23:23:10 +0530

    Create Area_of_a_triangle.c

---
+++ Area_of_a_triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, s, area;
+
+    printf("Enter the lengths of the three sides: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    s = (a + b + c) / 2;
+    area = sqrt(s * (s - a) * (s - b) * (s - c));
+
+    printf("The area of the triangle is: %.2lf\n", area);
+
+    return 0;
+}
