aa1d049d90e13e693a5023745e9b1a29c133ce3d
vedam-parthiv-kumar <vedamparthivkumar06@gmail.com>
2026-03-18 23:46:34 +0530

    Create Area_of_rectangle.c

---
+++ Area_of_rectangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float length, width, area;
+
+    printf("Enter length: ");
+    scanf("%f", &length);
+    printf("Enter width: ");
+    scanf("%f", &width);
+
+    area = length * width;
+
+    printf("Area: %.2f\n", area);
+
+    return 0;
+}
