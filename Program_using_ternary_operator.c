dcb419b7cdc039b269a60c5bda3416cd1f7322ba
vedam-parthiv-kumar <vedamparthivkumar06@gmail.com>
2026-03-26 23:46:30 +0530

    Create Program_using_ternary_operator.c

---
+++ Program_using_ternary_operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ float a,b,c,large;
+ printf("Enter any 3 numbers\n");
+ scanf("%f%f%f",&a,&b,&c);
+ large = a>b? (a>c?a:c): (b>c?b:c);
+ printf("The larger no is :%f\n", large);
+ return 0;
+}
